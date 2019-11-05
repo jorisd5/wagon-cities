8eaa727dcf61c02a49a76951ec947f07b6764222
Joris Dewaele <joris.dewaele@tutanota.com>
2019-11-05 12:27:52 +0100

    Added action for activeCity-onClick

--- src/containers/active_city.jsx
+++ src/containers/active_city.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 const ActiveCity = (props) => {
+  if (!props.activeCity) {
+    return (
+      <div className="active-city">
+        <p>Select a city...</p>
+      </div>
+    );
+  }
+
+  const url = `https://kitt.lewagon.com/placeholder/cities/${props.activeCity.slug}`;
+
   return (
     <div className="active-city">
-      <h1>{props.city.name}</h1>
-      <p>{props.city.address}</p>
+      <h1>{props.activeCity.name}</h1>
+      <p>{props.activeCity.address}</p>
+      <img src={url} alt="selected city" />
     </div>
   );
 };
 
-export default ActiveCity;
+function mapStateToProps(state) {
+  return {
+    activeCity: state.activeCity
+  };
+}
+
+export default connect(mapStateToProps)(ActiveCity);
 
--- src/containers/city.jsx
+++ src/containers/city.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-import { selectCity } from '../actions';
+import selectCity from '../actions';
 
 const City = (props) => {
   return (
